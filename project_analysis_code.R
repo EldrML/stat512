ff515c8c2b365ecd8236c1b8feb7d1af64783ff7
Liam <ldompro@gmail.com>
2020-12-01 12:08:43 -0500

    final code analysis

--- project_analysis_code.R
+++ project_analysis_code.R
@@ -10,6 +10,10 @@ numdata$PRP = NULL    #Our comparison regressor for the rest of the regressors.
 
 #Create linear model that is our comparison model.
 m_prp = lm(ERP~PRP,data=waredata)
+summary(m_prp)
+
+#Create a null model
+m_base = lm(ERP ~ 0, data=numdata)
 
 #Create a scatter matrix of the data to look for relevant trends.
 pairs(numdata)
@@ -18,21 +22,22 @@ pairs(numdata)
 MYCT_Tran = with(waredata, invTranEstimate(MYCT, ERP))
 
 #First Order Exhaustive Model Selection
-m_1stOrd = lm(ERP ~ . + I(MYCT^MYCT_Tran[1]$lambda), data=numdata)
+m_1stOrd = lm(ERP ~ . + I(MYCT^MYCT_Tran[1]$lambda) - MYCT, data=numdata)
 all_test2 = summary(regsubsets(formula(m_1stOrd), data = numdata))
 
 #Exhaustive Model selection: Generate a full model of all potentially interesting regressors.
-m_full = lm(ERP ~ . ^2 + I(MYCT^MYCT_Tran[1]$lambda) - MYCT, data=numdata)
+m_full = lm(ERP ~ . ^2 -1 + I(MYCT^MYCT_Tran[1]$lambda), data=numdata)
 all_test = summary(regsubsets(formula(m_full), data = numdata))
 all_test_r2 = all_test$adjr2 #Find the r^2 for each of the top 8 models of the data
 #Top 8 regressors model from Exhaustive search
-m_ex8 = lm(ERP ~ MMAX + CACH + I(MYCT^MYCT_Tran[1]$lambda) + MYCT:CHMAX + MMIN:MMAX + MMAX:CACH + MMAX:CHMAX + CACH:CHMAX, data = numdata)
+m_ex8 = lm(ERP ~ 0 + MMAX + MMIN + CACH + CHMAX + MYCT + I(MYCT^MYCT_Tran[1]$lambda) + MYCT:CHMAX + MMIN:MMAX + MMAX:CACH + MMAX:CHMAX + CACH:CHMAX, data = numdata)
 
 #Perform 3 types of model selection to look for the best models.
 m_step   = step(m_base, formula(m_full), direction="both")
 m_back   = step(m_full, direction="backward")
 m_for    = step(m_base, formula(m_full), direction="forward")
 
+#Summarize all the models
 summary(m_base)
 summary(m_full)
 summary(m_step)
@@ -66,8 +71,7 @@ bic_comp    = data.frame(BIC(m_base),
                          BIC(m_for),
                          BIC(m_ex8))
 
-#Selecting the best model: 
+#Selecting the best model, performing ANOVA, and getting a summary.
 m_best = m_ex8 #Choosing the simplest model because the difference between it and the more complex models in terms of R^2 is negligible.
-anova(m_best)
+anova(m_base, m_best)
 summary(m_best)
-print.Anova( anova(m_base, m_full, m_step, m_back, m_for, m_ex8))
