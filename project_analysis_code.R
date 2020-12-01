2a2bef73f759bfe666392979811a3065525855cf
Liam <ldompro@gmail.com>
2020-11-30 22:24:40 -0500

    Selected the "best" model and generated a summary/anova table for it

--- project_analysis_code.R
+++ project_analysis_code.R
@@ -17,18 +17,28 @@ pairs(numdata)
 #Appears the MYCT has a non-linear relation to PRP and ERP, perform a transformation estimate.
 MYCT_Tran = with(waredata, invTranEstimate(MYCT, ERP))
 
-#Exhaustive Model selection: Generate a full model of all potentially interesting regressors.
-m_full = lm(ERP ~ . ^2 + I(MYCT^MYCT_Tran[1]$lambda), data=numdata)
+#First Order Exhaustive Model Selection
+m_1stOrd = lm(ERP ~ . + I(MYCT^MYCT_Tran[1]$lambda), data=numdata)
+all_test2 = summary(regsubsets(formula(m_1stOrd), data = numdata))
 
-#Find the r^2 for each of the top 8 models of the data
+#Exhaustive Model selection: Generate a full model of all potentially interesting regressors.
+m_full = lm(ERP ~ . ^2 + I(MYCT^MYCT_Tran[1]$lambda) - MYCT, data=numdata)
 all_test = summary(regsubsets(formula(m_full), data = numdata))
-all_test_r2 = all_test$adjr2
+all_test_r2 = all_test$adjr2 #Find the r^2 for each of the top 8 models of the data
+#Top 8 regressors model from Exhaustive search
 m_ex8 = lm(ERP ~ MMAX + CACH + I(MYCT^MYCT_Tran[1]$lambda) + MYCT:CHMAX + MMIN:MMAX + MMAX:CACH + MMAX:CHMAX + CACH:CHMAX, data = numdata)
 
 #Perform 3 types of model selection to look for the best models.
 m_step   = step(m_base, formula(m_full), direction="both")
 m_back   = step(m_full, direction="backward")
 m_for    = step(m_base, formula(m_full), direction="forward")
+
+summary(m_base)
+summary(m_full)
+summary(m_step)
+summary(m_back)
+summary(m_for)
+summary(m_ex8)
 
 #Generate Comparison Dataframes
 rsqr_comp   = data.frame(summary(m_base)$adj.r.squared, 
@@ -49,7 +59,15 @@ aic_comp    = data.frame(AIC(m_base),
                          AIC(m_back),
                          AIC(m_for),
                          AIC(m_ex8))
+bic_comp    = data.frame(BIC(m_base), 
+                         BIC(m_full),
+                         BIC(m_step),
+                         BIC(m_back),
+                         BIC(m_for),
+                         BIC(m_ex8))
 
-
-
+#Selecting the best model: 
+m_best = m_ex8 #Choosing the simplest model because the difference between it and the more complex models in terms of R^2 is negligible.
+anova(m_best)
+summary(m_best)
 print.Anova( anova(m_base, m_full, m_step, m_back, m_for, m_ex8))
